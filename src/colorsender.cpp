3f0f3575c7de3c606f0bdc0f0f8cc54127a851ff
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2020-06-08 15:58:42 +0200

    Update transmitter handling

--- src/colorsender.cpp
+++ src/colorsender.cpp
@@ -2,21 +2,18 @@
 
 #include <QDebug>
 
-ColorSender::ColorSender(QWidget *parent,
-                         std::shared_ptr<MuebTransmitter> transmitter)
-    : QColorDialog(parent) {
+ColorSender::ColorSender(QWidget *parent)
+    : QColorDialog(parent), m_transmitter(MuebTransmitter::getInstance()) {
   setOptions(QColorDialog::DontUseNativeDialog | QColorDialog::NoButtons);
 
   setWindowFlags(windowFlags() | Qt::MSWindowsFixedSizeDialogHint);
 
   connect(this, &ColorSender::currentColorChanged, this,
           &ColorSender::colorChanged);
-
-  if (!transmitter) m_transmitter = std::make_shared<MuebTransmitter>(this);
 }
 
 void ColorSender::colorChanged(const QColor &color) {
   QImage frame{libmueb::defaults::frame};
   frame.fill(color);
-  m_transmitter->sendFrame(frame);
+  m_transmitter.sendFrame(frame);
 }
--- src/colorsender.h
+++ src/colorsender.h
@@ -2,18 +2,17 @@
 #define COLOR_SENDER_H
 
 #include <QColorDialog>
-#include <memory>
+
 #include "muebtransmitter.h"
 
 class ColorSender : public QColorDialog {
   Q_OBJECT
 
  public:
-  ColorSender(QWidget* parent = nullptr,
-              std::shared_ptr<MuebTransmitter> transmitter = nullptr);
+  ColorSender(QWidget* parent = nullptr);
 
  private:
-  std::shared_ptr<MuebTransmitter> m_transmitter{nullptr};
+  MuebTransmitter& m_transmitter;
 
   void colorChanged(const QColor& color);
 };
